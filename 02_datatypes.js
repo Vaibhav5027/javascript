85c5436030e930249e0d0d07886757746b3025e5
Vaibhav Patil <vaibhavpatil5027@gmail.com>
2024-10-05 18:19:19 +0530

    datatypes in js

---
+++ 02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict";
+//if you you use ..."USE STRICT" at start then engine will undersatnd the code will wriiten in newer verson
+
+// DataTypes
+boolean
+string 
+number
+bigint
+//null===>its a standalone value 
+undefined
+
+/*if you want to check the dtatatypes of the varible then you can  use
+TypesOf TMethod
+
+but 
+console.log(typeof null); it will give you Object as datatype not a nulll in other
+cases it will give particular type as datatype*/
