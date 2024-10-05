604a53153d4475457c86b1a109d51d748552b6a9
Vaibhav Patil <vaibhavpatil5027@gmail.com>
2024-10-05 18:22:20 +0530

    String in jS

---
+++ 05_String.js
@@ -0,0 +1,11 @@
+
+// way to define String
+const fname = new String("vaibhav");
+const lname = "patil";
+
+console.log(fname.charAt(2));
+console.log(fname.toUpperCase());
+console.log(fname.indexOf('v'));
+
+// there are plenty of strings method
+
