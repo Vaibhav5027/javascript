cd0c5113dfb509bce735821efd773e75133447df
Vaibhav Patil <vaibhavpatil5027@gmail.com>
2024-10-05 18:21:19 +0530

    stack and heap memory concept

---
+++ 04_memorymanagement.js
@@ -0,0 +1,19 @@
+
+/* thre are two types of memory STACK and HEAP
+
+
+
+all the primitive(string,number,null,undefined,bigint,symbol,boolean) datatypes are stored in stack memory 
+and other non-primitive(array,object,function) datatypes are stored on heap meamory
+
+if you assign the variable to another varible and after that you made some changes on this then it will not 
+reflect in first varible
+
+for exapme==>
+    let name=vaibhav;
+let newname=name;
+newname="sham";
+it will not affect the name vaiable name varible stiil have the value vaibhav  ,,
+
+but this is not case in non primituve datatpes
+*/
