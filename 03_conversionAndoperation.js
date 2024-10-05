7ae9d3a831f7c866e4ec70f33b288b9590580445
Vaibhav Patil <vaibhavpatil5027@gmail.com>
2024-10-05 18:20:29 +0530

    conversion and operation

---
+++ 03_conversionAndoperation.js
@@ -0,0 +1,24 @@
+let age = "33"
+let ageInNumber = Number(age);
+// console.log(ageInNumber);
+// console.log(typeof ageInNumber);
+
+let boolean = 1;
+let convertedBoolean = Boolean(1);
+// console.log(convertedBoolean);
+// console.log(typeof convertedBoolean);
+/*
+1==>true,
+0==>false
+""==>empty string==> false
+"abc"==>true
+*/
+
+let number = "33"
+let numberInString = String(number)
+// console.log(numberInString);
+// console.log(typeof numberInString);
+
+
+
+//******************Opearations*******************************/
